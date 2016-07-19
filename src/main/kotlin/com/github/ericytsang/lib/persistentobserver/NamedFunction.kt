75261a2e89b28654af57eed0c06e8ed65c6e9093
Eric Tsang <surplus.et@gmail.com>
2016-07-18 17:00:33 -0700

    added receiver to the block to make it possible to access the named function within the block lambds
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- src/main/kotlin/com/github/ericytsang/lib/persistentobserver/NamedFunction.kt
+++ src/main/kotlin/com/github/ericytsang/lib/persistentobserver/NamedFunction.kt
@@ -8,13 +8,13 @@ import java.io.Serializable
 interface NamedFunction<Transaction:TransactionAdapter,in Argument:Serializable>
 {
     val name:String?
-    val block:(transaction:Transaction,argument:Argument) -> Unit
+    val block:NamedFunction<*,*>.(transaction:Transaction,argument:Argument) -> Unit
     val shouldEnqueue:(list:List<ObservableManager.Command<Transaction>>,command:ObservableManager.Command<Transaction>,argument:Argument) -> Boolean
 }
 
 class SimpleNamedFunction<Transaction:TransactionAdapter,in Argument:Serializable>(
     override val name:String?,
-    override val block:(transaction:Transaction,argument:Argument) -> Unit)
+    override val block:NamedFunction<*,*>.(transaction:Transaction,argument:Argument) -> Unit)
 :NamedFunction<Transaction,Argument>
 {
     override val shouldEnqueue = fun(list:List<ObservableManager.Command<Transaction>>,command:ObservableManager.Command<Transaction>,argument:Argument):Boolean
@@ -25,7 +25,7 @@ class SimpleNamedFunction<Transaction:TransactionAdapter,in Argument:Serializabl
 
 class StaticNamedFunction<Transaction:TransactionAdapter,in Argument:Serializable>(
     override val name:String?,
-    override val block:(transaction:Transaction,argument:Argument) -> Unit)
+    override val block:NamedFunction<*,*>.(transaction:Transaction,argument:Argument) -> Unit)
 :NamedFunction<Transaction,Argument>
 {
     override val shouldEnqueue = fun(list:List<ObservableManager.Command<Transaction>>,command:ObservableManager.Command<Transaction>,argument:Argument):Boolean
